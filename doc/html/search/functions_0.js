4e7248ccb01b2938c45233ae67b7de666846c98d
DivyanshuSaxena <dssaxena2011@gmail.com>
2018-03-04 17:09:16 +0530

    new documentation done

--- doc/html/search/functions_0.js
+++ doc/html/search/functions_0.js
@@ -1,4 +1,6 @@
 var searchData=
 [
-  ['create3d',['create3D',['../class_projection2_d.html#a02e2cbca6d928a05400597693cbb6a08',1,'Projection2D']]]
+  ['create3d',['create3D',['../class_projection2_d.html#a02e2cbca6d928a05400597693cbb6a08',1,'Projection2D']]],
+  ['createobject',['createObject',['../_main_8cpp.html#a5c38baf978c399ae8cc04df06da85c43',1,'Main.cpp']]],
+  ['createprojection',['createProjection',['../_main_8cpp.html#a8b0b9e643edcb552ad38f8b687036659',1,'Main.cpp']]]
 ];
--- doc/html/search/functions_3.js
+++ doc/html/search/functions_3.js
@@ -1,5 +1,7 @@
 var searchData=
 [
   ['relativeposition',['relativePosition',['../class_point.html#a700201e1da138be7e3225b9d2bff50e9',1,'Point']]],
+  ['renderobject',['renderObject',['../_main_8cpp.html#ad0739e255d0ab5aaabeaea59a2e57f64',1,'Main.cpp']]],
+  ['renderprojection',['renderProjection',['../_main_8cpp.html#af9dfa01f76820fc1c4908c305ccb7903',1,'Main.cpp']]],
   ['rotateobject',['rotateObject',['../class_object3_d.html#a476b3de610cb30be0b050b4701ba4701',1,'Object3D']]]
 ];
